feb77e7338e5d711bb1c76a88a466fba4b21250e
jhb77 <josephbarrows03@gmail.com>
2025-04-08 10:52:22 -0500

    further changes

--- Project 4/prim.cpp
+++ Project 4/prim.cpp
@@ -3,9 +3,9 @@
  * prim.cpp
  * Spring 2025
  *
- * Partner 1:
- * Partner 2:
- * Date:
+ * Partner 1: Joseph Barrows
+ * Partner 2: Deven Platt
+ * Date: 4/16/25
  */
 
 #include "prim.hpp"
@@ -15,30 +15,94 @@ using namespace std;
 /*
  * getMin
  */
-int getMin(vector<double> &cost, vector<bool> &visited) {
+int getMin(vector<double> &cost, vector<bool> &visited)
+{
     return -1;
 }
 
 /*
  * isEmpty
  */
-bool isEmpty(vector<bool> &visited) {
+bool isEmpty(vector<bool> &visited)
+{
     return true;
 }
 
 /*
- * prim
+ * prim : Generates a minimum spanning tree of a graph using Prim's algorithm
+ *
+ * INPUTS
+ * vector<Vertex> &adjList : a list of vectors representing every vertex in the graph
+ * vector<double> &adjMat : a matrix that describes the cost of travelling from one vertex to any other vertex
+ *
+ * OUTPUTS
+ * vector<Edge> mst : an array of edge objects that represent the generated minimum spanning tree
  */
-vector<Edge> prim(vector<Vertex> &adjList, vector<double> &adjMat) {
+vector<Edge> prim(vector<Vertex> &adjList, vector<double> &adjMat)
+{
     // Initialize the empty MST.
     vector<Edge> mst;
-    
+
     // For each vertex, we need a visited flag, a 'cost' for Prim's, and the
     // prev values to track the MST. Store these in seperate vectors.
     int n = adjList.size();
     vector<bool> visited(n, false);
     vector<double> cost(n, numeric_limits<double>::infinity());
     vector<int> prev(n, -1);
+
+    // Loop through |v| - 1 times
+    // Since there will be |v| - 1 edges in the MST
+    int current = 0;
+    visited[current] = true;
+    for (int i = 0; i < n - 1; i++)
+    {
+        // Iterate through each edge leaving current and update the cost vector accordingly
+        for (int j = 0; j < n; j++)
+        {
+            // Check if the vertex is not the current vertex
+            // (To eliminate zero cost problem)
+            if (current != j)
+            {
+                // Update the cost vector if the current cost to get to the next vertex is less
+                double edge_cost = adjMat[(current * n) + j];
+                if (edge_cost < cost[j])
+                {
+                    cost[j] = edge_cost;
+
+                    // Update the previous value as well
+                    prev[j] = current;
+                }
+            }
+        }
+
+        // Iterate through the cost vector and select the next best edge
+        // (Linear search)
+        double min_cost = numeric_limits<double>::infinity();
+        int min_cost_vertex;
+        for (int j = 0; j < n; j++)
+        {
+            // If the cost to vertex j is less than the current mininmum cost
+            // And j has yet to be visited
+            // Then we want to set it as the next vertex to be visited
+            if (cost[j] < min_cost && visited[j] == false)
+            {
+                min_cost = cost[j];
+                min_cost_vertex = j;
+            }
+        }
+
+        // Add the minimum edge to the MST
+        Edge new_edge(adjList[min_cost_vertex], adjList[prev[min_cost_vertex]], min_cost);
+        mst.push_back(new_edge);
+
+        // Update MST neighbors
+        adjList[min_cost_vertex].mstNeighbors.push_back(prev[min_cost_vertex]);
+        adjList[prev[min_cost_vertex]].mstNeighbors.push_back(min_cost_vertex);
+
+        // Mark the min_cost_vertex as visited and set it to current
+        visited[min_cost_vertex] = true;
+        current = min_cost_vertex;
+    }
 
     return mst;
 }
--- Project 4/tsp.cpp
+++ Project 4/tsp.cpp
@@ -3,9 +3,9 @@
  * tsp.cpp
  * Spring 2025
  *
- * Partner 1:
- * Partner 2:
- * Date:
+ * Partner 1: Joseph Barrows
+ * Partner 2: Deven Platt
+ * Date: 4/16/25
  */
 
 #include "tsp.hpp"
@@ -13,15 +13,54 @@
 using namespace std;
 
 /*
- * tsp
+ * tsp: Creates a tour that visits each node of the graph a single time reducing the cost of that tour
+ *
+ * INPUTS
+ * vector<Vertex> &adjList : a list of vectors representing every vertex in the graph
+ * Vertex &start : the first vertex in the graph used to start tracing the tour
+ *
+ * OUTPUTS
+ * vector<int> tour : The TSP tour of the graph
  */
-vector<int> tsp(vector<Vertex> &adjList, Vertex &start) {
+vector<int> tsp(vector<Vertex> &adjList, Vertex &start)
+{
     // Create the empty tour.
     vector<int> tour;
-    
+
     // Create the vector for tracking visited during the DFS.
     int n = adjList.size();
     vector<bool> visited(n, false);
+
+    // Create a stack and push the start value onto it
+    // Also mark the start value as visited so we don't accidentally end the tour preemptively
+    stack<Vertex> s;
+    s.push(start);
+    visited[start.label] = true;
+
+    while (!s.empty())
+    {
+        // Take the top node off of the stack
+        Vertex current = s.top();
+        s.pop();
+
+        // Add the current vertex to the tour
+        tour.push_back(current.label);
+
+        // Iterate through the current vertex's neighbors
+        // Only add the ones that have not been visited to the stack
+        for (int neighbor : current.mstNeighbors)
+        {
+            if (visited[neighbor] == false)
+            {
+                // Add the neighbor to the stack and mark it as visited
+                s.push(adjList[neighbor]);
+                visited[neighbor] = true;
+            }
+        }
+    }
+
+    // Complete the tour by adding the starting vertex to the end
+    tour.push_back(start.label);
 
     return tour;
 }
